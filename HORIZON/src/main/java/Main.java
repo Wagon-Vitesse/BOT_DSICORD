2a3813f29dd61fe53ce3f8501a425d0c0657d508
Wagon-Vitesse <ozerkapar127@gmail.com>
2025-01-01 23:18:44 +0100

    1er commit

---
+++ HORIZON/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.managers.AudioManager;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        JDABuilder jdaBuilder = JDABuilder.createDefault("MTA1ODg5ODAyNDgxNTkxOTE2Nw.Grr7g1.mWOL5CSzlse-hhk9EsgueZs5XZyhO8Pu7-Q6-s");
+
+        jdaBuilder.build();
+
+    }
+}

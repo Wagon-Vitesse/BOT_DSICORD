95f3026290a73670a3bd7ad8faa6cd2b547c4f9e
Wagon-Vitesse <ozerkapar127@gmail.com>
2025-01-02 22:17:43 +0100

    Add command (!join, !leave, !play, !etem)

--- HORIZON/src/main/Main.java
+++ HORIZON/src/main/Main.java
@@ -1,5 +1,8 @@
+package main;
+
+import listeners.CommandListener;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.managers.AudioManager;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
 import java.io.FileInputStream;
@@ -18,10 +21,15 @@ public class Main {
         }catch (Exception e){
             e.printStackTrace();
         }
+
+
         JDABuilder jdaBuilder = JDABuilder.createDefault(TOKEN);
+        jdaBuilder
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES)
+                .addEventListeners(new CommandListener())
+                .build();
 
 
-        jdaBuilder.build();
 
 
     }
